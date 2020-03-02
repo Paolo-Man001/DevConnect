eb3daa165fdd94461af413695f30bbb4fc9e81f9
Paolo-Man001 <p.a.2manlunas@gmail.com>
2020-03-02 18:07:40 +1300

    Add connection option 'useUnifiedTopology: true' in db.js.

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
       await mongoose.connect(db, {
          useNewUrlParser: true,
          useCreateIndex: true,
-         useFindAndModify: false    // Always use this connection option
+         useFindAndModify: false,    // Always use this connection option
+         useUnifiedTopology: true
       });
 
       console.log('mongoDB Connected...');
